73875498849ad99f7f20bbf29271ff37d290c7e3
TrungHieu0 <146054601+TrungHieu0@users.noreply.github.com>
2023-10-24 20:28:10 +0700

    Update buoi6_bai3.py
    Signed-off-by: TrungHieu0 <146054601+TrungHieu0@users.noreply.github.com>

--- buoi6_bai3.py
+++ buoi6_bai3.py
@@ -2,27 +2,28 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Read the image
+# Đọc hình ảnh
 img = cv2.imread('test.png')
-# Hiển thị ảnh gốc
-        plt.subplot(1, 2, 1)
-        plt.title("Original")
-        plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-        plt.axis('off')
 
-# Convert the image to grayscale
-gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-# Normalize the grayscale image
-normalized_gray_img = cv2.normalize(gray_img, None, 0, 255, cv2.NORM_MINMAX)
-
-# Display the original image
+# Hiển thị hình ảnh gốc
 plt.subplot(1, 2, 1)
 plt.title("Original")
 plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
 plt.axis('off')
 
-# Display the normalized image
+# Chuyển đổi hình ảnh thành ảnh grayscale
+gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# Chuẩn hóa ảnh grayscale
+normalized_gray_img = cv2.normalize(gray_img, None, 0, 255, cv2.NORM_MINMAX)
+
+# Hiển thị hình ảnh gốc
+plt.subplot(1, 2, 1)
+plt.title("Original")
+plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+plt.axis('off')
+
+# Hiển thị ảnh đã được chuẩn hóa
 plt.subplot(1, 2, 2)
 plt.title("Normalized")
 plt.imshow(normalized_gray_img, cmap='gray')
